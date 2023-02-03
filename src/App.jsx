55625db76ab89b52cda9920e3ca95e9bf57258b2
Steveb Mendez <Stevenampaiz@gmail.com>
2023-02-02 23:57:14 -0600

    fix: :rocket: image path fixed for gh-pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ function App() {
   return (
     <>
       <Nav />
-      <LadingSection image="/assets/hand2.png" />
+      <LadingSection image="./assets/hand2.png" />
       <LadingSection
-        image="/assets/milk.png"
+        image="./assets/milk.png"
         title="MUSEUM OF CANDY"
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corporis recusandae dolor dicta veniam sapiente velit excepturi, possimus voluptate unde delectus harum assumenda culpa laborum quos impedit ipsam illo eum!"
         type="type1"
       ></LadingSection>
       <LadingSection
-        image="/assets/gumball.png"
+        image="./assets/gumball.png"
         title="MUSEUM OF CANDY"
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corporis recusandae dolor dicta veniam sapiente velit excepturi, possimus voluptate unde delectus harum assumenda culpa laborum quos impedit ipsam illo eum!"
         type="type2"
       ></LadingSection>
       <LadingSection
-        image="/assets/sprinkles.png"
+        image="./assets/sprinkles.png"
         title="MUSEUM OF CANDY"
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corporis recusandae dolor dicta veniam sapiente velit excepturi, possimus voluptate unde delectus harum assumenda culpa laborum quos impedit ipsam illo eum!"
         type="type1"
